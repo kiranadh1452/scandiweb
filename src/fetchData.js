873bd772f6ae6f41e8ab2a9c708808eef9dd75f8
Kiran Adhikari <kiranadh1452@gmail.com>
2022-04-25 22:41:07 +0545

    Update fetchData.js

--- src/fetchData.js
+++ src/fetchData.js
@@ -8,7 +8,6 @@ const fetchProductData = async () => {
   container.innerText = ""; // to clear the product listing
   const response = await fetch('https://scandiweb-kiranadh.000webhostapp.com/');
   const jsonData = await response.json();
-  console.log(jsonData);
   callMeNow(jsonData);
 }
 
